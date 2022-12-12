d6e775f82606f1b6adaf4eddc3afdd5d5b3f8f79
Kevin <kjriverac@ufpso.edu.co>
2022-12-12 13:39:26 -0500

    reajuste para listar,agregar,modificar,usuarios

--- js/usuario.js
+++ js/usuario.js
@@ -1,5 +1,4 @@
-// funciones js para el modelo de usuario 
-
+//funciones js para el modulo de usuarios
 const urlApi = "http://localhost:9000";//colocar la url con el puerto
 
 async function login(){
@@ -18,72 +17,12 @@ async function login(){
         body: JSON.stringify(jsonData)
     }
     const request = await fetch(urlApi+"/auth/login",settings);
-    //console.log(await request.text());
     if(request.ok){
-        
-        const respuesta = await request.text();
-        localStorage.token = respuesta;
-
-        //localStorage.token = respuesta;
-        localStorage.correo = jsonData.correo;      
+        const respuesta = await request.text();        
+        localStorage.token = respuesta;     
         location.href= "dashboard.html";
     }
 }
-/*async function login(){
-    let correo = document.querySelector('#myForm #correo').value;
-    let contraseña = document.querySelector('#myForm #password').value;
-    var jsonData = {
-        "correo":correo,
-        "password":contraseña,
-     
-        };
-    /*var myForm = document.getElementById("myForm");
-    var formData = new FormData(myForm);
-    var jsonData = {};
-    for(var [k, v] of formData){//convertimos los datos a json
-        jsonData[k] = v;
-    }*/
-    /*console.log(jsonData);
-
-    var settings={
-        method: 'POST',
-        headers:{
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(jsonData)
-    }
-    const request = await fetch(urlApi+"/auth/login",settings);
-    //console.log(await request.text());
-    if(request.ok){
-        Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'logeado exitosamente',
-            showConfirmButton: false,
-            timer: 1500
-          })
-        const respuesta = await request.text();
-       
-        localStorage.token = respuesta;
-        
-        //localStorage.token = respuesta;
-        localStorage.correo = jsonData.correo; 
-        setTimeout(function () {
-            location.href= "dashboard.html";
-        }, 2000);     
-        
-       
-    }else{
-        Swal.fire({
-            position: 'top-end',
-            icon: 'error',
-            text: 'los datos del usuario no son validos',
-            showConfirmButton: false,
-            timer: 3000
-          });
-    }
-}*/
 
 function listarUsuarios(){
     validaToken();
@@ -99,63 +38,52 @@ function listarUsuarios(){
     .then(response => response.json())
     .then(function(data){
         
-            var usuarios = '';
+            var usuarios = `
+            <div class="p-3 mb-2 bg-light text-dark">
+                    <h1 class="display-5"><i class="fa-solid fa-list"></i> Listado de usuarios</h1>
+                </div>
+                  
+                <a href="#" onclick="registerForm('true')" class="btn btn-outline-success"><i class="fa-solid fa-user-plus"></i></a>
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody id="listar">`;
             for(const usuario of data){
                 console.log(usuario.correo)
                 usuarios += `
-                <div class="card">
-                <div class="card-header">
-                    Listar productos del inventario
-                </div>
-                <div class="card-body">
-                   <div class="table-responsive">
-                    <table  class="table table-striped table-bordered" style="width:100%">
-                        <thead class="thead-light">
-                            <tr>
-                                <th style="text-align: center;">id usuario</th>
-                                <th style="text-align: center;">nombre</th>
-                                <th style="text-align: center;">apellidos</th>
-                                <th style="text-align: center;">documento</th>
-                                <th style="text-align: center;">correo</th>
-                                <th style="text-align: center;">Acciones</th>
-                            </tr>
-                        </thead>
-                        <tbody id="lista_productos">
-    
+                
                         <tr>
-                        <th scope="row">${usuario.id}</th>
-                        <td>${usuario.nombre}</td>
-                        <td>${usuario.apellidos}</td>
-                        <td>${usuario.documento}</td>
-                        <td>${usuario.correo}</td>
-                        <td>
-                        <button type="button" class="btn btn-outline-danger" 
-                        onclick="eliminaUsuario('${usuario.id}')">
-                            <i class="fa-solid fa-user-minus"></i>
-                        </button>
-                        <i class= "fas fa-edit" onclick="eliminaUsuario('${usuario.id}')" ></i>
-                        <a href="#" onclick="verModificarUsuario('${usuario.id}')" class="btn btn-outline-warning">
-                            <i class="fa-solid fa-user-pen"></i>
-                        </a>
-                        <a href="#" onclick="verUsuario('${usuario.id}')" class="btn btn-outline-info">
-                            <i class="fa-solid fa-eye"></i>
-                        </a>
-                        '</td>
-                        <br/>
-                    </tr>
-                           
-                                      
-                                    
-                                
-                            
-                            
-                        </tbody>
-                    </table>
-
-                    </div>
-                </div>
-            </div>`;
+                            <th scope="row">${usuario.id}</th>
+                            <td>${usuario.nombre}</td>
+                            <td>${usuario.apellidos}</td>
+                            <td>${usuario.correo}</td>
+                            <td>
+                            <button type="button" class="btn btn-outline-danger" 
+                            onclick="eliminaUsuario('${usuario.id}')">
+                                <i class="fa-solid fa-user-minus"></i>
+                            </button>
+                            <a href="#" onclick="verModificarUsuario('${usuario.id}')" class="btn btn-outline-warning">
+                                <i class="fa-solid fa-user-pen"></i>
+                            </a>
+                            <a href="#" onclick="verUsuario('${usuario.id}')" class="btn btn-outline-info">
+                                <i class="fa-solid fa-eye"></i>
+                            </a>
+                            </td>
+                        </tr>
+                    `;
+                
             }
+            usuarios += `
+            </tbody>
+                </table>
+            `;
             document.getElementById("datos").innerHTML = usuarios;
     })
 }
@@ -171,11 +99,11 @@ function eliminaUsuario(id){
         },
     }
     fetch(urlApi+"/usuario/"+id,settings)
-    .then(response => response.text())
-    .then(function(data){
+    .then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
         listarUsuarios();
         alertas("Se ha eliminado el usuario exitosamente!",2)
-    })
+      })
 }
 
 function verModificarUsuario(id){
@@ -188,7 +116,7 @@ function verModificarUsuario(id){
             'Authorization': localStorage.token
         },
     }
-     fetch(urlApi+"/usuario/"+id,settings)
+    fetch(urlApi+"/usuario/"+id,settings)
     .then(response => response.json())
     .then(function(usuario){
             var cadena='';
@@ -198,13 +126,13 @@ function verModificarUsuario(id){
                     <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Modificar Usuario</h1>
                 </div>
               
-                <form action="" method="post" id="myForm2">
+                <form action="" method="post" id="myForm">
                     <input type="hidden" name="id" id="id" value="${usuario.id}">
                     <label for="nombre" class="form-label">First Name</label>
                     <input type="text" class="form-control" name="nombre" id="nombre" required value="${usuario.nombre}"> <br>
                     <label for="apellidos"  class="form-label">Last Name</label>
                     <input type="text" class="form-control" name="apellidos" id="apellidos" required value="${usuario.apellidos}"> <br>
-                    <label for="documento" class="form-label">docuemnto</label>
+                    <label for="documento"  class="form-label">document</label>
                     <input type="text" class="form-control" name="documento" id="documento" required value="${usuario.documento}"> <br>
                     <label for="correo" class="form-label">correo</label>
                     <input type="correo" class="form-control" name="correo" id="correo" required value="${usuario.correo}"> <br>
@@ -223,7 +151,7 @@ function verModificarUsuario(id){
 
 async function modificarUsuario(id){
     validaToken();
-    var myForm = document.getElementById("myForm2");
+    var myForm = document.getElementById("myForm");
     var formData = new FormData(myForm);
     var jsonData = {};
     for(var [k, v] of formData){//convertimos los datos a json
@@ -269,6 +197,7 @@ function verUsuario(id){
                     <li class="list-group-item">Nombre: ${usuario.nombre}</li>
                     <li class="list-group-item">Apellido: ${usuario.apellidos}</li>
                     <li class="list-group-item">Correo: ${usuario.correo}</li>
+                    <li class="list-group-item">Documento: ${usuario.documento}</li>
                 </ul>`;
               
             }
@@ -279,14 +208,14 @@ function verUsuario(id){
 }
 
 function alertas(mensaje,tipo){
-    var color ="";
+    var color ="warning";
     if(tipo == 1){//success verde
         color="success"
     }
     else{//danger rojo
         color = "danger"
     }
-    var alerta =`<div class="alert alert-'+color+' alert-dismissible fade show" role="alert">
+    var alerta =`<div class="alert alert-${color} alert-dismissible fade show" role="alert">
                     <strong><i class="fa-solid fa-triangle-exclamation"></i></strong>
                         ${mensaje}
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -294,39 +223,39 @@ function alertas(mensaje,tipo){
     document.getElementById("alerta").innerHTML = alerta;
 }
 
-
-function registerForm(){
+function registerForm(auth=false){
     cadena = `
             <div class="p-3 mb-2 bg-light text-dark">
                 <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Registrar Usuario</h1>
             </div>
               
-            <form action="" method="post" id="myForm1">
+            <form action="" method="post" id="myFormReg">
                 <input type="hidden" name="id" id="id">
                 <label for="nombre" class="form-label">First Name</label>
                 <input type="text" class="form-control" name="nombre" id="nombre" required> <br>
                 <label for="apellidos"  class="form-label">Last Name</label>
                 <input type="text" class="form-control" name="apellidos" id="apellidos" required> <br>
-                <label for="documento"  class="form-label">documento</label>
+                <label for="documento"  class="form-label">document</label>
                 <input type="text" class="form-control" name="documento" id="documento" required> <br>
                 <label for="correo" class="form-label">correo</label>
-                <input type="text" class="form-control" name="correo" id="correo" required> <br>
+                <input type="correo" class="form-control" name="correo" id="correo" required> <br>
                 <label for="password" class="form-label">Password</label>
                 <input type="password" class="form-control" id="password" name="password" required> <br>
-                <button type="button" class="btn btn-outline-info" onclick="registrarUsuario()">Registrar</button>
+                <button type="button" class="btn btn-outline-info" onclick="registrarUsuario('${auth}')">Registrar</button>
             </form>`;
             document.getElementById("contentModal").innerHTML = cadena;
             var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
             myModal.toggle();
 }
 
-async function registrarUsuario(){
-    var myForm = document.getElementById("myForm1");
+async function registrarUsuario(auth=false){
+    var myForm = document.getElementById("myFormReg");
     var formData = new FormData(myForm);
     var jsonData = {};
     for(var [k, v] of formData){//convertimos los datos a json
         jsonData[k] = v;
     }
+    console.log("data user ",jsonData);
     const request = await fetch(urlApi+"/usuario", {
         method: 'POST',
         headers:{
@@ -334,9 +263,15 @@ async function registrarUsuario(){
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(jsonData)
+    })
+    .then(response => response.json())
+    .then(function(respuesta){
+        console.log("respuesta peticion", respuesta)
     });
-    //listarUsuarios();
-    //alertas("Se ha registrado el usuario exitosamente!",1)
+    if(auth){
+        listarUsuarios();
+    }
+    alertas("Se ha registrado el usuario exitosamente!",1)
     document.getElementById("contentModal").innerHTML = '';
     var myModalEl = document.getElementById('modalUsuario')
     var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
