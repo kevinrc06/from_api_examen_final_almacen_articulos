789d7143288aa06f134b07c72757952d0f80a43e
Kevin <kjriverac@ufpso.edu.co>
2022-12-12 16:09:53 -0500

    de los articulos, tenemos la lista de ellos y la funcion de eliminar

---
+++ js/articulo.js
@@ -0,0 +1,289 @@
+//funciones js para el modulo de usuarios
+const urlApi3 = "http://localhost:9000";//colocar la url con el puerto
+
+
+
+function listarArticulos(){
+    validaToken();
+    var settings={
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+    }
+    fetch(urlApi3+"/articulos",settings)
+    .then(response => response.json())
+    .then(function(data){
+        
+            var articulos = `
+            <div class="p-3 mb-2 bg-light text-dark">
+                    <h1 class="display-5"><i class="fa-solid fa-list"></i> Listado de articulos</h1>
+                </div>
+                  
+                <a href="#" onclick="registerForm3('true')" class="btn btn-outline-success"><i class="fa-solid fa-user-plus"></i></a>
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">codigo</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">descripcion</th>
+                        <th scope="col">fecha_registro</th>
+                        <th scope="col">stock</th>
+                        <th scope="col">categoria</th>
+                        <th scope="col">usuario</th>
+                        <th scope="col">precio_venta</th>
+                        <th scope="col">precio_compra</th>
+                        <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody id="listar">`;
+            for(const articulo of data){
+                //console.log(usuario.correo)
+                articulos += `
+                
+                        <tr>
+                            <th scope="row">${articulo.id}</th>
+                            <td>${articulo.codigo}</td>
+                            <td>${articulo.nombre}</td>
+                            <td>${articulo.descripcion}</td>
+                            <td>${articulo.fecha_registro}</td>
+                            <td>${articulo.stock}</td>
+                            <td>${articulo.categoria.nombre}</td>
+                            <td>${articulo.usuario.nombre}</td>
+                            <td>${articulo.precio_venta}</td>
+                            <td>${articulo.precio_compra}</td>
+                            <td>
+                            <button type="button" class="btn btn-outline-danger" 
+                            onclick="eliminaArticulo('${articulo.codigo}')">
+                                <i class="fa-solid fa-user-minus"></i>
+                            </button>
+                            <a href="#" onclick="verModificarArticulo('${articulo.codigo}')" class="btn btn-outline-warning">
+                                <i class="fa-solid fa-user-pen"></i>
+                            </a>
+                            <a href="#" onclick="verArticulo('${articulo.codigo}')" class="btn btn-outline-info">
+                                <i class="fa-solid fa-eye"></i>
+                            </a>
+                            </td>
+                        </tr>
+                    `;
+                
+            }
+            articulos += `
+            </tbody>
+                </table>
+            `;
+            document.getElementById("datos").innerHTML = articulos;
+    })
+}
+
+function eliminaArticulo(codigo){
+    validaToken();
+    var settings={
+        method: 'DELETE',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+    }
+    fetch(urlApi3+"/articulo/"+codigo,settings)
+    .then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+        listarArticulos();
+        alertas("Se ha eliminado el usuario exitosamente!",2)
+      })
+}
+
+function verModificarUsuario(id){
+    validaToken();
+    var settings={
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+    }
+    fetch(urlApi3+"/usuario/"+id,settings)
+    .then(response => response.json())
+    .then(function(usuario){
+            var cadena='';
+            if(usuario){                
+                cadena = `
+                <div class="p-3 mb-2 bg-light text-dark">
+                    <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Modificar Usuario</h1>
+                </div>
+              
+                <form action="" method="post" id="myForm">
+                    <input type="hidden" name="id" id="id" value="${usuario.id}">
+                    <label for="nombre" class="form-label">First Name</label>
+                    <input type="text" class="form-control" name="nombre" id="nombre" required value="${usuario.nombre}"> <br>
+                    <label for="apellidos"  class="form-label">Last Name</label>
+                    <input type="text" class="form-control" name="apellidos" id="apellidos" required value="${usuario.apellidos}"> <br>
+                    <label for="documento"  class="form-label">document</label>
+                    <input type="text" class="form-control" name="documento" id="documento" required value="${usuario.documento}"> <br>
+                    <label for="correo" class="form-label">correo</label>
+                    <input type="correo" class="form-control" name="correo" id="correo" required value="${usuario.correo}"> <br>
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" required> <br>
+                    <button type="button" class="btn btn-outline-warning" 
+                        onclick="modificarUsuario('${usuario.id}')">Modificar
+                    </button>
+                </form>`;
+            }
+            document.getElementById("contentModal").innerHTML = cadena;
+            var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
+            myModal.toggle();
+    })
+}
+
+async function modificarUsuario(id){
+    validaToken();
+    var myForm = document.getElementById("myForm");
+    var formData = new FormData(myForm);
+    var jsonData = {};
+    for(var [k, v] of formData){//convertimos los datos a json
+        jsonData[k] = v;
+    }
+    const request = await fetch(urlApi3+"/usuario/"+id, {
+        method: 'PUT',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+        body: JSON.stringify(jsonData)
+    });
+    listarUsuarios();
+    alertas("Se ha modificado el usuario exitosamente!",1)
+    document.getElementById("contentModal").innerHTML = '';
+    var myModalEl = document.getElementById('modalUsuario')
+    var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
+    modal.hide();
+}
+
+function verUsuario(id){
+    validaToken();
+    var settings={
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+    }
+    fetch(urlApi3+"/usuario/"+id,settings)
+    .then(response => response.json())
+    .then(function(usuario){
+            var cadena='';
+            if(usuario){                
+                cadena = `
+                <div class="p-3 mb-2 bg-light text-dark">
+                    <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Visualizar Usuario</h1>
+                </div>
+                <ul class="list-group">
+                    <li class="list-group-item">Nombre: ${usuario.nombre}</li>
+                    <li class="list-group-item">Apellido: ${usuario.apellidos}</li>
+                    <li class="list-group-item">Correo: ${usuario.correo}</li>
+                    <li class="list-group-item">Documento: ${usuario.documento}</li>
+                </ul>`;
+              
+            }
+            document.getElementById("contentModal").innerHTML = cadena;
+            var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
+            myModal.toggle();
+    })
+}
+
+function alertas(mensaje,tipo){
+    var color ="warning";
+    if(tipo == 1){//success verde
+        color="success"
+    }
+    else{//danger rojo
+        color = "danger"
+    }
+    var alerta =`<div class="alert alert-${color} alert-dismissible fade show" role="alert">
+                    <strong><i class="fa-solid fa-triangle-exclamation"></i></strong>
+                        ${mensaje}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                 </div>`;
+    document.getElementById("alerta").innerHTML = alerta;
+}
+
+function registerForm(auth=false){
+    cadena = `
+            <div class="p-3 mb-2 bg-light text-dark">
+                <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Registrar Usuario</h1>
+            </div>
+              
+            <form action="" method="post" id="myFormReg">
+                <input type="hidden" name="id" id="id">
+                <label for="nombre" class="form-label">First Name</label>
+                <input type="text" class="form-control" name="nombre" id="nombre" required> <br>
+                <label for="apellidos"  class="form-label">Last Name</label>
+                <input type="text" class="form-control" name="apellidos" id="apellidos" required> <br>
+                <label for="documento"  class="form-label">document</label>
+                <input type="text" class="form-control" name="documento" id="documento" required> <br>
+                <label for="correo" class="form-label">correo</label>
+                <input type="correo" class="form-control" name="correo" id="correo" required> <br>
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" required> <br>
+                <button type="button" class="btn btn-outline-info" onclick="registrarUsuario('${auth}')">Registrar</button>
+            </form>`;
+            document.getElementById("contentModal").innerHTML = cadena;
+            var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
+            myModal.toggle();
+}
+
+async function registrarUsuario(auth=false){
+    var myForm = document.getElementById("myFormReg");
+    var formData = new FormData(myForm);
+    var jsonData = {};
+    for(var [k, v] of formData){//convertimos los datos a json
+        jsonData[k] = v;
+    }
+    console.log("data user ",jsonData);
+    const request = await fetch(urlApi3+"/usuario", {
+        method: 'POST',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsonData)
+    })
+    .then(response => response.json())
+    .then(function(respuesta){
+        console.log("respuesta peticion", respuesta)
+    });
+    if(auth){
+        listarUsuarios();
+    }
+    alertas("Se ha registrado el usuario exitosamente!",1)
+    document.getElementById("contentModal").innerHTML = '';
+    var myModalEl = document.getElementById('modalUsuario')
+    var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
+    modal.hide();
+}
+
+function modalConfirmacion(texto,funcion){
+    document.getElementById("contenidoConfirmacion").innerHTML = texto;
+    var myModal = new bootstrap.Modal(document.getElementById('modalConfirmacion'))
+    myModal.toggle();
+    var confirmar = document.getElementById("confirmar");
+    confirmar.onclick = funcion;
+}
+
+function salir(){
+    localStorage.clear();
+    location.href = "index.html";
+}
+
+function validaToken(){
+    if(localStorage.token == undefined){
+        salir();
+    }
+}
